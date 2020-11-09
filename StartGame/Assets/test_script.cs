06cfce2c2037e65fd77cdb85fa8348c9eff70c35
houjuu0079 <73355778+houjuu0079@users.noreply.github.com>
2020-11-09 15:41:26 +0900

    testup3

--- StartGame/Assets/test_script.cs
+++ StartGame/Assets/test_script.cs
@@ -7,7 +7,7 @@ public class test_script : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.Log("test");
-		
+        Debug.Log("test2UP");
 	}
 	
 	// Update is called once per frame
